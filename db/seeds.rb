b97027d0e364c10f1b41a496b2d5efef347b00c1
Kyle Kwon <kyleskwon@gmail.com>
2015-10-20 21:08:54 -0700

    Incomplete; added new/unique  post and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,18 @@ include RandomData
  50.times do
 
    Post.create!(
-
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
+
+ puts "#{Post.count}"
+    Post.find_or_create_by(
+      title: "Blue's clues",
+      body: "She's a clueless dog"
+      )
+ puts "#{Post.count}"
 
  # Create Comments
 
@@ -20,6 +26,14 @@ include RandomData
      body: RandomData.random_paragraph
    )
  end
+
+ puts "#{Comment.count}"
+    Comment.find_or_create_by(
+      post: posts.sample,
+      body: "She's a sweetheart"
+      )
+ puts "#{Comment.count}"
+
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
