050f22b1fa167dde1e7a590542205365edf8c182
Kyle Kwon <kyleskwon@gmail.com>
2015-10-21 17:33:06 -0700

    checkpoint re-completed; seeded random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,21 @@
 include RandomData
+#Create Posts
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+    )
+end
+posts = Post.all
 
- # Create Posts
- 50.times do
+#Create Comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+    )
+end
 
-   Post.create!(
-
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
-
- # Create Comments
-
- 100.times do
-   Comment.create!(
-
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
-
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,29 +1,27 @@
 module RandomData
- # #6
-   def random_paragraph
-     sentences = []
-     rand(4..6).times do
-       sentences << random_sentence
-     end
 
-     sentences.join(" ")
-   end
+    def random_paragraph
+      sentences = []
+      rand(4..6).times do
+        sentences << random_sentence
+      end
 
- # #7
-   def random_sentence
-     strings = []
-     rand(3..8).times do
-       strings << random_word
-     end
+      sentences.join(" ")
+    end
 
-     sentence = strings.join(" ")
-     sentence.capitalize << "."
-   end
+    def random_sentence
+      strings = []
+      rand(3..8).times do
+        strings << random_word
+      end
 
- # #8
-   def random_word
-     letters = ('a'..'z').to_a
-     letters.shuffle!
-     letters[0,rand(3..8)].join
-   end
- end
+      sentence = strings.join(" ")
+      sentence.capitalize << "."
+    end
+
+    def random_word
+      letters = ('a'..'z').to_a
+      letters.shuffle!
+      letters[0,rand(3..8)].join
+    end
+  end
