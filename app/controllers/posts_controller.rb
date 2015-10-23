7df621775b1cd12099276f954ce02a4295cf21f1
Kyle Kwon <kyleskwon@gmail.com>
2015-10-23 16:42:56 -0700

    able to read posts, need to fix create; no button on posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
