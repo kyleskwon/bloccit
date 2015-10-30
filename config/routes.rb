6c64c71c8d2388138e8220423b3c8356bf3636a5
Kyle Kwon <kyleskwon@gmail.com>
2015-10-30 16:46:02 -0700

    assignment complete; added confirm view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :topics
-  resources :posts
+
   resources :topics do
     resources :posts, except: [:index]
   end
 
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' => 'users#confirm'
+
   get 'about' => 'welcome#about'
+  
   root to: 'welcome#index'
 end
