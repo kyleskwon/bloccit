c6355812c2ec5046cffe5bbdb2f4db7dc243e07b
Kyle Kwon <kyleskwon@gmail.com>
2015-11-04 22:45:27 -0800

    checkpoint complete; added labels

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,9 @@ include RandomData
 RSpec.describe Topic, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-  it { should have_many(:posts) }
+   it { should have_many(:posts) }
+   it { should have_many(:labelings) }
+   it { should have_many(:labels).through(:labelings) }
 
    it { should validate_presence_of(:name) }
    it { should validate_presence_of(:description) }
