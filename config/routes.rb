19e09641432ed0dda1749eb2e3fbe0b0329f4b03
Kyle Kwon <kyleskwon@gmail.com>
2015-11-13 14:28:30 -0800

    Checkpoint complete

--- config/routes.rb
+++ config/routes.rb
@@ -18,13 +18,13 @@ Rails.application.routes.draw do
   end
 
   get 'about' => 'welcome#about'
-  
+
   root to: 'welcome#index'
 
   namespace :api do
      namespace :v1 do
-       resources :users, only: [:index, :show]
-       resources :topics, only: [:index, :show]
+       resources :users, only: [:index, :show, :create, :update]
+       resources :topics, except: [:edit, :new]
      end
    end
 end
