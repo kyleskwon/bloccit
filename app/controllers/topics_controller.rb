56fd7a7672b6db8a409ad1e1cff30cc559c0a104
Kyle Kwon <kyleskwon@gmail.com>
2016-06-02 05:49:13 -0700

    minor grammar errors

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  # before_action :authorize_user, except: [:index, :show]
 
   def index
     @topics = Topic.all
@@ -28,7 +28,7 @@ class TopicsController < ApplicationController
       flash[:notice] = "Topic was saved successfully."
       redirect_to @topic
     else
-      flash[:error] = "Erro creating topic. Please try again."
+      flash[:error] = "Error creating topic. Please try again."
       redirect_to :new
     end
   end
@@ -70,11 +70,11 @@ class TopicsController < ApplicationController
      params.require(:topic).permit(:name, :description, :public)
    end
 
-   def authorize_user
-     unless current_user.admin?
-       flash[:error] = "You must be an admin to do that."
-       redirect_to topics_path
-     end
-   end
+  #  def authorize_user
+  #    unless current_user.admin?
+  #      flash[:error] = "You must be an admin to do that."
+  #      redirect_to topics_path
+  #    end
+  #  end
 
 end
