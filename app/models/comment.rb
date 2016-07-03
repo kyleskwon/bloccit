c9251a3f3bc96bd3b1d21ac44769b285e7842c89
Kyle Kwon <kyleskwon@gmail.com>
2016-07-02 22:31:40 -0400

    refactor welcome index

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  validates :body, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 1 }, presence: true
   validates :user, presence: true
 
   default_scope { order('updated_at DESC') }
